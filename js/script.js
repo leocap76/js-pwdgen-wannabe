1712f51adb2e75d7af77e7901ba0fa0eb59f4e9d
Leonardo Capogna <leonardocapogna@gmail.com>
2020-11-10 16:21:50 +0100

    da collegare lo script

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var surname = prompt("Nome");
+
+var surname = prompt("Cognome");
+
+var color = prompt("Colore preferito");
+
+var password = name + surname + color;
+
+document.getElementById('name').innerHTML = name;
+
+document.getElementById('surname').innerHTML = surname;
+
+document.getElementById('color').innerHTML = color;
+
+document.getElementById('password').innerHTML = name + surname + color;
+document.getElementById('password').innerHTML = password;
